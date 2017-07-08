2bdcada6c38f956f09fddfa5edc2316ed89b3f52
RyoYoneyama <ryo@Ryo-MacBook-Pro.local>
2017-07-08 19:36:00 +0900

    add strip function

--- strutil.c
+++ strutil.c
@@ -21,3 +21,41 @@ int get_delim_pos(char* str, char *delim)
 		return (int)(pos - str);
 	}
 }
+
+void lstrip(char* str) {
+	int i = 0, j = 0;
+
+	for(i = 0; str[i] != '\0'; i++) {
+		if(str[i] != ' '){
+			break;
+		}
+	}
+
+	if(str[i] == '\0') return;
+
+	j = 0;
+	while(str[i + j] != '\0') {
+		str[j] = str[i + j];
+		j++;
+	}
+	str[j] = '\0';
+
+	return;
+}
+
+void rstrip(char* str) {
+	int i = 0;
+	int len = strlen(str);
+
+	for(i = len - 1; str[i] == ' '; i--) {
+		str[i] = '\0';
+	}
+
+	return;
+}
+
+void strip(char *str) {
+	lstrip(str);
+	rstrip(str);
+	return;
+}
--- strutil.h
+++ strutil.h
@@ -4,4 +4,8 @@
 void convert_to_lower(char* str);
 int get_delim_pos(char* str, char *delim);
 
+void strip(char* str);
+void lstrip(char* str);
+void rstrip(char* str);
+
 #endif
