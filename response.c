9d0cc364ea79852dd4c05658feead43a39d3c0f3
RyoYoneyama <ryo@Ryo-MacBook-Pro.local>
2017-07-09 10:51:53 +0900

    modify to create response header correctly

--- response.c
+++ response.c
@@ -1,6 +1,8 @@
 #include "http.h"
 #include "response.h"
 #include "strutil.h"
+#include "debug.h"
+
 #include <string.h>
 #include <stdio.h>
 #include <unistd.h>
@@ -35,6 +37,8 @@ int add_response_header(response_info *info, char* key, char* value)
 	item->value = (char*)calloc(sizeof(char), strlen(value) + 1);
 	strcpy(item->value, value);
 
+	item->next = NULL;
+
 	if(info->headers == NULL) {
 		info->headers = item;
 	} else {
@@ -58,21 +62,82 @@ int set_response_body(response_info *info, char* body, size_t size)
 	return 0;
 }
 
+#define BUFF_UNIT 1024
 int send_response(int fd, response_info *info)
 {
-	char message[1024];
-	memset(message, 0, sizeof(message));
+	size_t buf_size = BUFF_UNIT;
+	size_t buf_len  = 0;
+	char *send_buff = (char*)calloc(sizeof(char), buf_size);
 
-	sprintf(message, "%s %d %s\r\n%s : %s\r\n\r\n%s",
-					info->status_line.version,
-					info->status_line.code,
-					info->status_line.phrase,
-					info->headers->key,
-					info->headers->value,
-					info->body);
+	char status_line[512];
+	size_t status_size;
 
-	write(fd, message, strlen(message));
+	char header[512];
+	size_t header_size;
+
+	size_t crlf_size;
+	size_t body_size;
+
+	memset(status_line, 0, sizeof(status_line));
+	sprintf(status_line, "%s %d %s"CRLF, info->status_line.version, info->status_line.code, info->status_line.phrase);
+	status_size = strlen(status_line);
+
+	/* create buffer if needed */
+	while(buf_size - buf_len <= status_size){
+		buf_size += BUFF_UNIT;
+		send_buff = (char*)realloc(send_buff, sizeof(char) * buf_size);
+	}
+
+	/* set status line of response */
+	DPRINTF("--- status line ---\n%s", status_line);
+	memcpy(send_buff + buf_len, status_line, status_size);
+	buf_len += status_size;
+
+	/* add header */
+	DPRINTF("%s", "--- response header ---\n");
+	header_list *item = info->headers;
+	while(item != NULL) {
+		memset(header, 0, sizeof(header));
+		sprintf(header, "%s:%s"CRLF, item->key, item->value);
+		header_size = strlen(header);
+
+		while(buf_size - buf_len <= header_size){
+			buf_size += BUFF_UNIT;
+			send_buff = (char*)realloc(send_buff, sizeof(char) * buf_size);
+		}
+
+		DPRINTF("%s", header);
+		memcpy(send_buff + buf_len, header, header_size);
+		buf_len += header_size;
+
+		item = item->next;
+	}
+
+	/* add delimiter between header and body */
+	crlf_size = strlen(CRLF);
+	while(buf_size - buf_len <= crlf_size){
+		buf_size += BUFF_UNIT;
+		send_buff = (char*)realloc(send_buff, sizeof(char) * buf_size);
+	}
+
+	memcpy(send_buff + buf_len, CRLF, crlf_size);
+	buf_len += crlf_size;
+
+	/* add body */
+	body_size = strlen(info->body);
+	while(buf_size - buf_len <= body_size){
+		buf_size += BUFF_UNIT;
+		send_buff = (char*)realloc(send_buff, sizeof(char) * buf_size);
+	}
+
+	DPRINTF("--- response body ---\n%s\n", info->body);
+	memcpy(send_buff + buf_len, info->body, body_size);
+	buf_len += body_size;
+
+	write(fd, send_buff, buf_len);
+	free(send_buff);
 
 	return 0;
 }
+#undef BUFF_UNIT
 
